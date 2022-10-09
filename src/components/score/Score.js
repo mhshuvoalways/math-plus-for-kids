42b4f9a3dddda16736f233413746e69b179cf4b6
mlhnshuvo <mlhnshuvo@gmail.com>
2022-10-09 21:04:29 +0600

    chagen

--- src/components/score/Score.js
+++ src/components/score/Score.js
@@ -14,78 +14,80 @@ const Score = ({ doitagainHandler, answare }) => {
   });
 
   return (
-    <div className="overview">
-      <div
-        className="score-score"
-        style={{ backgroundImage: `url(${ScoreImg})` }}
-      >
-        <p className="score-score-number">{score}</p>
-        <p className="score-score-text">Your score</p>
-      </div>
-      <div className="score-score-msg">
-        <p className="score-score-gb">Great Job!</p>
-        <p className="score-score-des">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        </p>
-      </div>
-      <div className="overview-score">
-        <div className="overview-card">
-          <p className="score-completion-number">
-            {(100 / mathLength) * score}%
+    <div>
+      <div className="overview">
+        <div
+          className="score-score"
+          style={{ backgroundImage: `url(${ScoreImg})` }}
+        >
+          <p className="score-score-number">{score}</p>
+          <p className="score-score-text">Your score</p>
+        </div>
+        <div className="score-score-msg">
+          <p className="score-score-gb">Great Job!</p>
+          <p className="score-score-des">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           </p>
-          <p className="score-completion-complete">Completion</p>
         </div>
-        <div className="overview-card">
-          <div className="score-completion-number score__total-number">
-            <p className="score-total-number-point"></p>
-            <p>{mathLength}</p>
-          </div>
-          <p className="score-completion-complete">Total Questions</p>
-        </div>
-        <div className="overview-card">
-          <div className="score-completion-number score__total-correct">
-            <p className="score-total-number-point score-total-number-point__correct"></p>
-            <p>{score}</p>
-          </div>
-          <p className="score-completion-complete">Correct answers</p>
-        </div>
-        <div className="overview-card">
-          <div className="score-completion-number score__total-wrong">
-            <p className="score-total-number-point score-total-number-point__wrong"></p>
-            <p>{mathLength - score}</p>
-          </div>
-          <p className="score-completion-complete">Wrong answers</p>
-        </div>
-      </div>
-      <div className="short-overview">
-        {answare.map((answare) => (
-          <div className="short-overview-items" key={answare._id}>
-            <div className="short-overview-items-check">
-              <div>
-                <img
-                  src={answare.isCurrect ? RightImg : WrongImg}
-                  alt=""
-                  className="short-overview-items-icon"
-                />
-              </div>
-              <div>
-                <p>{`${answare.firstNumber} ${answare.oparation} ${answare.secondNumber} = ${answare.userAnsware}`}</p>
-                {answare.isCurrect ? null : (
-                  <p className="short-overview-items-check-right-ans">
-                    Right answer: {answare.currectAnsware}
-                  </p>
-                )}
-              </div>
-            </div>
-            <p className="short-overview-sec-dec">
-              Solved in{" "}
-              <span className="short-overview-sec-dec__number">
-                {answare.second}
-              </span>{" "}
-              Sec
+        <div className="overview-score">
+          <div className="overview-card">
+            <p className="score-completion-number">
+              {(100 / mathLength) * score}%
             </p>
+            <p className="score-completion-complete">Completion</p>
           </div>
-        ))}
+          <div className="overview-card">
+            <div className="score-completion-number score__total-number">
+              <p className="score-total-number-point"></p>
+              <p>{mathLength}</p>
+            </div>
+            <p className="score-completion-complete">Total Questions</p>
+          </div>
+          <div className="overview-card">
+            <div className="score-completion-number score__total-correct">
+              <p className="score-total-number-point score-total-number-point__correct"></p>
+              <p>{score}</p>
+            </div>
+            <p className="score-completion-complete">Correct answers</p>
+          </div>
+          <div className="overview-card">
+            <div className="score-completion-number score__total-wrong">
+              <p className="score-total-number-point score-total-number-point__wrong"></p>
+              <p>{mathLength - score}</p>
+            </div>
+            <p className="score-completion-complete">Wrong answers</p>
+          </div>
+        </div>
+        <div className="short-overview">
+          {answare.map((answare) => (
+            <div className="short-overview-items" key={answare._id}>
+              <div className="short-overview-items-check">
+                <div>
+                  <img
+                    src={answare.isCurrect ? RightImg : WrongImg}
+                    alt=""
+                    className="short-overview-items-icon"
+                  />
+                </div>
+                <div>
+                  <p>{`${answare.firstNumber} ${answare.oparation} ${answare.secondNumber} = ${answare.userAnsware}`}</p>
+                  {answare.isCurrect ? null : (
+                    <p className="short-overview-items-check-right-ans">
+                      Right answer: {answare.currectAnsware}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <p className="short-overview-sec-dec">
+                Solved in{" "}
+                <span className="short-overview-sec-dec__number">
+                  {answare.second}
+                </span>{" "}
+                Sec
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       <motion.div
         className="score-doit-btn"
